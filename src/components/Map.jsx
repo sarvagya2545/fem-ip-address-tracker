28cbc60715b86e0b3d108a7ca54d3e13500d63a5
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-07-24 19:01:30 +0530

    basic map setup with marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,28 +1,50 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
 
 const Map = () => {
     const mapRef = useRef(null);
+    const markerRef = useRef(null);
 
     useEffect(() => {
         console.log('useeffect called');
-        if(!mapRef.current) {
-            const myMap = L.map('ipMap').setView([0,0], 1);
-            console.log(myMap);
-            let accessToken = 'pk.eyJ1Ijoic2FydmFneWEyNTQ1IiwiYSI6ImNrcmhuZ2F3ajBteW4ycHBrY3YzdjU5anQifQ.csBvrjRKTrh4ZqKn0RZXNw';
-            let url = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`;
-            const config = {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                id: 'mapbox/streets-v11',
-                accessToken: accessToken
-            }
-    
-            const tiles = L.tileLayer(url, config);
-            tiles.addTo(myMap);
-            
-            mapRef.current = myMap;
-        } 
+        if (!mapRef.current) 
+            createMap();
+
+        if(mapRef.current && !markerRef.current)
+            createMarker();
     }, []);
+
+    const createMap = () => {
+        const myMap = L.map('ipMap').setView([0, 0], 1);
+        let accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+        let url = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`;
+        const config = {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/streets-v11',
+            accessToken: accessToken
+        }
+
+        const tiles = L.tileLayer(url, config);
+        tiles.addTo(myMap);
+
+        mapRef.current = myMap;
+    }
+
+    const createMarker = () => {
+        const markerIcon = L.icon({
+            iconUrl: '/images/icon-location.svg',
+            shadowUrl: '',
+            iconSize: [20,25],
+            iconAnchor: [10, 25],
+            popupAnchor: [0, -12.5]
+        });
+
+
+        const marker = L.marker([51.5, -0.09], { icon: markerIcon })
+            .addTo(mapRef.current)
+            .bindPopup('I am somewhere');
+        markerRef.current = marker;
+    }
 
     return (
         <div className="map-container">
@@ -30,5 +52,5 @@ const Map = () => {
         </div>
     );
 }
- 
+
 export default Map;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv';
 import reactRefresh from '@vitejs/plugin-react-refresh'
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  define: {
+    "process.env.MAPBOX_ACCESS_TOKEN": JSON.stringify(process.env.REACT_APP_MAPBOX_ACCESS_TOKEN)
+  }
 })
